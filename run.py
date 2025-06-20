35acd14b0cf1bbf905d6f4ff28b4808f68b24af4
MEERAN2314 <meeran231422@gmail.com>
2025-06-21 02:11:44 +0530

    Update run.py

--- run.py
+++ run.py
@@ -354,7 +354,7 @@ def cleanup_cache():
         question_cache.clear()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
         
     app.run(debug=True)
